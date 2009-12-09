1a78d5371b5a807e3b6c0584d8115b34268571e5
monsat <tnk@daijiten.info>
2009-12-09 15:57:50 +0900

    deleted common actions

--- controllers/book_stand_authors_controller.php
+++ controllers/book_stand_authors_controller.php
@@ -2,28 +2,5 @@
 class BookStandAuthorsController extends BookStandAppController {
 
 	var $name = 'BookStandAuthors';
-
-	function index() {
-		$this->BookStandAuthor->recursive = 0;
-		$this->set('bookStandAuthors', $this->paginate());
-	}
-
-
-	function admin_view($id = null) {
-		if (!$id) {
-			$this->flash(__('Invalid BookStandAuthor', true), array('action'=>'index'));
-		}
-		$this->set('bookStandAuthor', $this->BookStandAuthor->read(null, $id));
-	}
-
-	function admin_delete($id = null) {
-		if (!$id) {
-			$this->flash(__('Invalid BookStandAuthor', true), array('action'=>'index'));
-		}
-		if ($this->BookStandAuthor->del($id)) {
-			$this->flash(__('BookStandAuthor deleted', true), array('action'=>'index'));
-		}
-	}
-
+	
 }
-?>
